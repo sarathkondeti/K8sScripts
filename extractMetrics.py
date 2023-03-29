d5713231831821c6c26fd5beaf940d80833baf91
sarathkondeti <sarathchandra1999@gmail.com>
2023-03-29 02:27:18 -0400

    new changes

--- extractMetrics.py
+++ extractMetrics.py
@@ -35,7 +35,7 @@ ncu_cmd = "ncu -c 5 -f -o ncu_report "
 ncu_cmd += "--metric smsp__inst_executed.avg.per_cycle_active,smsp__sass_average_branch_targets_threads_uniform.pct,lts__t_sector_hit_rate.pct,smsp__thread_inst_executed_per_inst_executed.ratio "
 ncu_cmd += "--kernel-name regex:"
 
-ncu_csv = "ncu --import  ncu_report.ncu-rep --csv > ncu.csv"
+ncu_csv = "ncu --import  ncu_report.ncu-rep --csv"
 ncu_csv = ncu_csv.split()
 for kernel in range(kernels):
     kernel_name = df["Name"][kernel]
@@ -44,4 +44,5 @@ for kernel in range(kernels):
     cli_cmd = ncu_cmd + kernel_regex
     cli_cmd = cli_cmd.split() + app_cmd
     subprocess.call(cli_cmd)
-    subprocess.call(ncu_csv)
+    with open('ncu.csv', 'w') as f:
+        process = subprocess.Popen(ncu_csv, stdout=f)
