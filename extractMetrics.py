91cd5041c632772c1e634721b425328ceb21828c
sarathkondeti <sarathchandra1999@gmail.com>
2023-03-28 20:57:40 -0400

    new changes

---
+++ extractMetrics.py
@@ -0,0 +1,8 @@
+#imports
+
+
+
+# nsys profile parsing
+#nsys profile -f true -o report ./heartwall ../../data/heartwall/test.avi 1
+
+# nsys stats --report gpukernsum --format csv --output lol report1.qdrep
