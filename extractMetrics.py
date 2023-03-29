9b4ed469912dec9896c99826d286f4822b3e8de5
sarathkondeti <sarathchandra1999@gmail.com>
2023-03-29 16:28:29 -0400

    new changes

--- extractMetrics.py
+++ extractMetrics.py
@@ -2,19 +2,35 @@
 import subprocess
 import sys
 import pandas as pd
+#parameters
+metrics = "smsp__inst_executed.avg.per_cycle_active,"
+metrics+= "smsp__sass_average_branch_targets_threads_uniform.pct,"
+metrics+= "lts__t_sector_hit_rate.pct,"
+metrics+= "smsp__thread_inst_executed_per_inst_executed.ratio"
+
+
+# helper functions
+def processKernel():
+    metrics_names = metrics.split(',');
+    metrics_values=[]
+    df = pd.read_csv("ncu.csv")
+    for metric in metrics_names:
+        _df = df["Name" = metric];
+        print(_df)
+
+# -----------------------------------------------------------------
 # Get the app run command
 app_cmd = sys.argv[1:]
 if len(app_cmd)==0:
     print("please provide app run cmd as argument")
     exit();
-
+# -----------------------------------------------------------------
 # nsys profile parsing
 nsys_prof = "nsys profile -f true -o nsys_prof"
 cli_cmd = nsys_prof.split(' ') + app_cmd;
 print("1/3 running nsys profile")
 print(cli_cmd)
 subprocess.call(cli_cmd)
-
 # nsys kernel stats
 nsys_stats = "nsys stats --report gpukernsum "
 nsys_stats += "--force-overwrite true --force-export true "
@@ -23,20 +39,20 @@ cli_cmd = nsys_stats.split()
 print("2/3 running nsys stats kernel")
 print(cli_cmd)
 subprocess.call(cli_cmd)
-
 # print top ~5 kernels
 df = pd.read_csv("nsys_stat_gpukernsum.csv")
 print(df.head(5))
 print("Enter the #kernels for profiling: ",end='');
 kernels = int(input())
-
+# ------------------------------------------------------------------
 # collect ncu Metrics
 ncu_cmd = "ncu -c 5 -f -o ncu_report "
-ncu_cmd += "--metric smsp__inst_executed.avg.per_cycle_active,smsp__sass_average_branch_targets_threads_uniform.pct,lts__t_sector_hit_rate.pct,smsp__thread_inst_executed_per_inst_executed.ratio "
+ncu_cmd += "--metric " + metrics
 ncu_cmd += "--kernel-name regex:"
 
 ncu_csv = "ncu --import  ncu_report.ncu-rep --csv"
 ncu_csv = ncu_csv.split()
+finalOutput = "app_output.csv"
 for kernel in range(kernels):
     kernel_name = df["Name"][kernel]
     kernel_regex = kernel_name.split('(<',1)[0]
@@ -46,3 +62,4 @@ for kernel in range(kernels):
     subprocess.call(cli_cmd)
     with open('ncu.csv', 'w') as f:
         process = subprocess.Popen(ncu_csv, stdout=f)
+    processKernel()
