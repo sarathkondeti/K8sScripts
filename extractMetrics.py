0501f14dba61341c367d240974be2d0be69632b2
sarathkondeti <sarathchandra1999@gmail.com>
2023-03-28 22:17:39 -0400

    new changes

--- extractMetrics.py
+++ extractMetrics.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import subprocess
 import sys
+import pandas as pd
 # Get the app run command
 app_cmd = sys.argv[1:]
 if len(app_cmd)==0:
@@ -20,3 +21,9 @@ cli_cmd = nsys_stats.split()
 print("2/3 running nsys stats kernel")
 print(cli_cmd)
 subprocess.call(cli_cmd)
+
+# print top ~5 kernels
+df = pd.read_csv("nsys_stat_gpukernsum.csv")
+print(df.head(5))
+print("Enter the #kernels for profiling: ",end='');
+kernels = int(input)
