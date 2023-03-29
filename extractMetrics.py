06d1816e03b9a43ecde60531c62cb396938ad69f
sarathkondeti <sarathchandra1999@gmail.com>
2023-03-28 21:28:34 -0400

    new changes

--- extractMetrics.py
+++ extractMetrics.py
@@ -8,8 +8,8 @@ print(app_cmd)
 
 # nsys profile parsing
 #nsys profile -f true -o report ./heartwall ../../data/heartwall/test.avi 1
-nsys_prof = "nsys profile -f true -o nsys_report "
-subprocess.call([nsys_prof, app_cmd])
+nsys_prof = "profile -f true -o nsys_report "
+subprocess.call(["nsys", nsys_prof, app_cmd])
 
 
 # nsys stats --report gpukernsum --format csv --output lol report1.qdrep
